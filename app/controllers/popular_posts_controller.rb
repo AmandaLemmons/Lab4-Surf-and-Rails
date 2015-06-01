16263d6f69ad090849a1c06f44089db8d1ed9ff2
Amanda Lemmons <amandalemmons@Amandas-MacBook-Pro.local>
2015-06-01 13:37:35 -0500

    changing pictures

--- app/controllers/popular_posts_controller.rb
+++ app/controllers/popular_posts_controller.rb
@@ -1,26 +1,34 @@
 class PopularPostsController < ApplicationController
 
+  class Post
+    attr_reader :name, :photo
+    def initialize(name, photo)
+      @name = name
+      @photo = photo
+    end
+  end
+
   before_action do
-    @post_name = ["Camel Rides Are The Coolest", "Follow The Leader, Leader, Leader", "Spice Up Your Life in Marrakesh", "Oh, A Cool Post in the Desert"]
-    @pics = ['another-cool-picture.jpg', 'img2.JPG', 'spices.JPG',  'village.JPG']
+
+    @post_name = []
+    @post_name << Post.new("Camel Rides Are The Coolest" ,'another-cool-picture.jpg')
+    @post_name << Post.new("Follow The Leader, Leader, Leader" ,"img2.JPG")
+    @post_name << Post.new("Sit back and let us lead you" ,"ad.JPG")
+    @post_name << Post.new("Spice Up Your Life in Marrakesh" ,"spices.JPG")
+    @post_name << Post.new("Oh, A Cool Post in the Desert" ,"village.JPG")
   end
 
 
 
   def index
+    @post = @post_name.first
+    render :show
   end
 
   def show
-    @popular_posts = params[:post_name]
-
-    @header =  @post_name.find do |post|
-    post == params[:post_name]
-
+    @post = @post_name.find do |post|
+      post.name == params[:post_name]
     end
-
-
-
-
   end
 
 
